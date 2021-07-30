d80e8c29d1ffa97173038f9e4e23030739d36855
Nicole Maurice <nicole.maurice0622@gmail.com>
2021-07-30 14:51:26 -0500

    new commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const menu = document.querySelector('#mobile_menu')
+const menuLinks = document.querySelector('.navbar_menu')
+const navLogo = document.querySelector('#navbar_logo')
+const body = document.querySelector('body')
+
+// Display Mobile menu
+const mobileMenu = () =>{
+  menu.classList.toggle('is-active')
+  menuLinks.classList.toggle('active')
+  body.classList.toggle('active')
+};
+// triggers navbar_toggle class 3 bars
+menu.addEventListener('click',mobileMenu)
+
+//Animations
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from('.animate-intro',{
+  duration:0.6,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3
+});
+
+gsap.from('.animate-gallery',{
+  scrollTrigger:'.animate-gallery',
+  duration:0.6,
+  opacity: 1.2,
+  y: -150,
+  stagger: 0.12
+});
+
+gsap.from('.animate-gallery-img',{
+  scrollTrigger:'.animate-gallery-img',
+  duration:0.6,
+  opacity: 1.2,
+  y: 150,
+  stagger: 0.12,
+  delay:0.5
+});
+
+gsap.from('.animate-subscription',{
+  scrollTrigger:'.animate-subscription',
+  duration:0.6,
+  opacity: 1.2,
+  y: -150,
+  stagger: 0.12
+});
+
+gsap.from('.animate-card',{
+  scrollTrigger:'.animate-card',
+  duration:0.5,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 1
+});
+
+gsap.from('.animate-form',{
+  scrollTrigger:'.animate-form',
+  duration:1,
+  opacity: 0,
+  y: -150,
+  stagger: 0.3,
+  delay: 0.2
+});
